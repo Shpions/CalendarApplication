7756a3a02d913ed1ba3fe95594c2fc4ec265da97
Shpions <xxiii@ukr.net>
2015-04-30 13:12:02 +0300

    com

--- src/client/src/ClientMain.java
+++ src/client/src/ClientMain.java
@@ -8,11 +8,8 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.rmi.RemoteException;
-import java.util.logging.Logger;
 
 public class ClientMain {
-
-    public static final Logger logger = Logger.getAnonymousLogger();
 
     public static void main(String[] args) throws RemoteException {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring-client.xml");
